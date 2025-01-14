7be51e90a7070ea6be9ec9cc9d688354c3104c08
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-14 12:44:13 -0500

    Update MainActivity.kt

--- app/src/main/java/com/loading/xcall/MainActivity.kt
+++ app/src/main/java/com/loading/xcall/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.loading.xcall
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
