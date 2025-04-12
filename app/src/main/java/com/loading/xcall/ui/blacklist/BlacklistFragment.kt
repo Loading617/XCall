5b7bd3f30b02e05f5b05e58aacf8adc9ecdc3ca4
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-11 22:58:08 -0400

    Update BlacklistFragment.kt

--- app/src/main/java/com/loading/xcall/ui/blacklist/BlacklistFragment.kt
+++ app/src/main/java/com/loading/xcall/ui/blacklist/BlacklistFragment.kt
@@ -15,8 +15,6 @@ class BlacklistFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
