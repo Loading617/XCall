0165a8bcf7919b8d7089e5b48937a5d37c714e26
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-11 22:58:57 -0400

    Update BlacklistViewModel.kt

--- app/src/main/java/com/loading/xcall/ui/blacklist/BlacklistViewModel.kt
+++ app/src/main/java/com/loading/xcall/ui/blacklist/BlacklistViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class BlacklistViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Blacklist Fragment"
     }
     val text: LiveData<String> = _text
 }
