decba5e61d70fba4cc95ef47c13a0e45d35301d9
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-15 21:24:34 -0400

    Add files via upload

---
+++ app/src/main/java/com/loading/xcall/data/lookup/NumberLookupService.kt
@@ -0,0 +1,36 @@
+package com.loading.xcall.data.lookup
+
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class NumberLookupService(private val context: Context) {
+
+    // Simulate a local list of spam numbers for now
+    private val localSpamNumbers = listOf(
+        "+18005551234",
+        "+1234567890",
+        "+1987654321"
+    )
+
+    suspend fun lookupNumber(number: String): LookupResult {
+        return withContext(Dispatchers.IO) {
+            try {
+                if (isInLocalBlacklist(number)) {
+                    LookupResult.SPAM
+                } else {
+                    // In future, do remote API call here
+                    LookupResult.UNKNOWN
+                }
+            } catch (e: Exception) {
+                Log.e("NumberLookupService", "Lookup failed", e)
+                LookupResult.UNKNOWN
+            }
+        }
+    }
+
+    private fun isInLocalBlacklist(number: String): Boolean {
+        return localSpamNumbers.contains(number.trim())
+    }
+}
